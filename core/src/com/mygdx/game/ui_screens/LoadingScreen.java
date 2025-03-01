9401eea162b0a05f181a8e9ae1384add2720fb59
Ungureanu <ungureanu.eduard2001@gmail.com>
2025-03-01 18:57:08 +0200

    Progressively color apples based on loading progress and animated duck
    - Modified LoadingScreen to gradually transition apples from gray to red based on asset loading progress.
    - Added a duck animation for the LoadingScreen

--- core/src/com/mygdx/game/ui_screens/LoadingScreen.java
+++ core/src/com/mygdx/game/ui_screens/LoadingScreen.java
@@ -1,7 +1,11 @@
 package com.mygdx.game.ui_screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.utilities_resources.Assets;
 
@@ -9,35 +13,69 @@ import com.mygdx.game.utilities_resources.Assets;
 public class LoadingScreen extends AbstractScreen {
 
     private final MyGdxGame game;
-    private float timeElapsed = 0;
-    private final float loadTime = 2.0f;
     private Assets assets;
+    private final Texture duckTexture;
+    private final Texture appleTexture;
+    protected Animation<TextureRegion> duckAnimation;
+    private final SpriteBatch batch;
+    private float stateTime;
 
     public LoadingScreen(MyGdxGame game) {
         super(game);
         this.game = game;
+        duckTexture = new Texture(Gdx.files.internal("loadingDuck.png"));
+        appleTexture = new Texture(Gdx.files.internal("Environment/apple.png"));
+        TextureRegion[][] tmp = TextureRegion.split(duckTexture, 128, 128);
+        TextureRegion[] characterFrames = new TextureRegion[4];
+        System.arraycopy(tmp[0], 0, characterFrames, 0, 4);
+        duckAnimation = new Animation<>(0.1f, characterFrames);
+        stateTime = 0f;
+        batch = new SpriteBatch();
     }
 
     @Override
     public void show() {
         assets = new Assets();
         assets.loadGameAssets();
-        assets.getAssetManager().finishLoading();
-
     }
 
     @Override
     public void render(float delta) {
-        timeElapsed += delta;
+        ScreenUtils.clear(0, 0, 0, 1);
 
-        Gdx.gl.glClearColor(.1f, .1f, .15f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stateTime += delta;
 
-        if (timeElapsed >= loadTime) {
-            // After loading time, switch to MainMenuScreen
+        TextureRegion currentFrame = duckAnimation.getKeyFrame(stateTime, true);
+        float duckX = Gdx.graphics.getWidth() / 2f - 100;
+        float duckY = Gdx.graphics.getHeight() / 2f;
+        float appleSize = 80;
+        float spacing = 10;
+        float startX = duckX - (2 * (appleSize + spacing));
+        float appleY = duckY - 120;
+
+        float progress = assets.getAssetManager().getProgress();
+        int totalApples = 6;
+        int coloredApples = (int) (progress * totalApples);
+
+        batch.begin();
+        batch.draw(currentFrame, duckX, duckY, 200, 200);
+
+        for (int i = 0; i < totalApples; i++) {
+            if (i < coloredApples) {
+                batch.setColor(1, 1, 1, 1);
+            } else {
+                batch.setColor(0.5f, 0.5f, 0.5f, 1);
+            }
+            batch.draw(appleTexture, startX + i * (appleSize + spacing), appleY, appleSize, appleSize);
+        }
+
+        batch.setColor(1, 1, 1, 1);
+        batch.end();
+        if (assets.getAssetManager().update()) {
             game.setScreen(new MainMenuScreen(game , assets));
         }
     }
+
 
     @Override
     public void resize(int i, int i1) {
@@ -45,5 +83,12 @@ public class LoadingScreen extends AbstractScreen {
 
     @Override
     public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        duckTexture.dispose();
+        appleTexture.dispose();
     }
 }
